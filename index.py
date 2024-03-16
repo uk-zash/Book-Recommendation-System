dd66f0a3e3a82f8f9bd1054cb2d31eebcc12c319
uk-zash <ssik938@gmail.com>
2024-03-16 21:23:33 +0530

    Error template added

--- index.py
+++ index.py
@@ -30,23 +30,51 @@ def recommend_ui():
 @app.route('/recommend_books',methods=['post'])
 def recommend():
     user_input = request.form.get('user_input')
-    index = np.where(pt.index == user_input)[0][0]
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[0:7]
+    try:
+        index = np.where(pt.index == user_input)[0][0]
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[0:7]
 
-    data = []
-    for i in similar_items:
-        item = []
-        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-L'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Year-Of-Publication'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Publisher'].values))
-        
-        data.append(item)
-    print(data)
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-L'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Year-Of-Publication'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Publisher'].values))
 
-    return render_template('recommend.html',data=data)
+            data.append(item)
+        print(data)
+
+        return render_template('recommend.html', data=data)
+    except IndexError:
+        return render_template('error.html',
+                               book_name = list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-L'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_rating'].values)
+                           )
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('error.html',
+                           book_name = list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-L'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_rating'].values)
+                           ), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('error.html',
+                           book_name = list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-L'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_rating'].values)
+                           ), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
